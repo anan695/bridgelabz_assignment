81234dc2fa8ef93ed3b3befb13f8878424ac1dde
anan65 <pandeyananyajyoti@gmail.com>
2025-11-07 14:17:39 +0530

    add function.java folder

---
+++ function.java
@@ -0,0 +1,103 @@
+// import java.util.*;
+
+// public class function {
+//     public static void printMyName(String[] args) {
+//         System.out.println("name");
+//         return;
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         String name =sc.next();
+
+//         printMyName(name);
+        
+//     }
+    
+// }
+
+
+//array ka question
+// import java.util.*;
+// public class function {
+
+//     public static void main(String[] args) {
+//         Scanner sc =new Scanner(System.in);
+//         int[] oneDArray={10,20,30,40,50};
+//         int[] a = new int[oneDArray.lengh];
+//         for(int i=0;i<oneDArray.length;i++){
+//             System.out.println("Enter the data");
+//             int data=sc.nextInt();
+//             a[i]=data;
+//         }
+//     }
+// }
+
+
+//make a function to add two numbers and return the sum
+// import java.util.*;
+// public class function {
+//     public static int calculateSum(int a,int b){
+//         int sum=a+b;
+//         return sum;
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int a=sc.nextInt();
+//         int b=sc.nextInt();
+
+//         int sum=calculateSum(a,b);
+//         System.out.println(sum);
+//     }
+// }
+
+
+//make a function to multiply two numbers and return their product
+// import java.util.*;
+// public class function {
+//     public static int calculateProduct(int a,int b){
+//         int product=a*b;
+//         return product;
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner sc =new Scanner(System.in);
+//         int a=sc.nextInt();
+//         int b=sc.nextInt();
+
+//         int product=calculateProduct(a, b);
+//         System.out.println(product);
+//     }
+// }
+
+//find the factorial number
+// import java.util.*;
+// public class function {
+//     public static void printFactorial(int n){
+//         if(n<0){
+//             System.out.println("Invalid Number");
+//             return;
+//         }
+//         int factorial=1;
+//         for(int i=n;i>=1;i--){
+//             factorial=factorial*i;
+//         }
+//         System.out.println(factorial);
+//         return;
+
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n=sc.nextInt();
+
+//         printFactorial(n);
+//     }
+// }
+
+
+
+
+
+
+
